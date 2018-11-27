cbcf64c9ef1d73a46b0c65e612ee30543faadb3b
Alex Carrega <contact@alexcarrega.com>
2018-11-27 19:28:51 +0100

    Stable relase

--- src/display.mjs
+++ src/display.mjs
@@ -1,9 +1,5 @@
 import $ from 'chalk';
 
-export const NOT = 'NOT';
-export const NEW_LINE = `\n`;
-export const ARROW = $.cyan('>>>');
-
 export const styleInfo = text => $.magenta(text);
 export const styleWarn = text => $.yellow(text);
 export const styleError = text => $.red(text);
@@ -14,13 +10,16 @@ export const styleText = text => $.white(text);
 
 export const styleHighlight = text => $.bold(text);
 
+export const NOT = 'NOT';
+export const NEW_LINE = `\n`;
+export const ARROW = $.cyan('>') + $.cyan('>') + styleText('>');
+
 export const info = text => console.log(NEW_LINE + styleInfo(text) + NEW_LINE);
 export const warn = text => console.log(NEW_LINE + styleWarn(text) + NEW_LINE);
 export const error = text => console.log(NEW_LINE + styleError(text + NEW_LINE));
 
 export const head = text => console.log(NEW_LINE + styleHead(text) + NEW_LINE);
 export const text = text => console.log(NEW_LINE + styleText(text) + NEW_LINE);
-
 
 export default {
    style: {
